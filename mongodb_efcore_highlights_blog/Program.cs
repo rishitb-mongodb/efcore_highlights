1843fe17f99fdca87006e82d77de0ca4eee73915
Luce Carter <luce@lucyscarter.co.uk>
2024-08-15 15:54:35 +0100

    Stripped back code to the required to start following the highlights in the blog post

--- mongodb_efcore_highlights_blog/Program.cs
+++ mongodb_efcore_highlights_blog/Program.cs
@@ -2,10 +2,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
-using MongoDB.EntityFrameworkCore;
 using MongoDB.EntityFrameworkCore.Extensions;
-using System.ComponentModel.DataAnnotations;
-using MongoDB.Bson.Serialization.Conventions;
 
 var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
 
@@ -14,69 +11,12 @@ if (connectionString == null)
     Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string");
     Environment.Exit(0);
 }
-
 var client = new MongoClient(connectionString);
-var database = client.GetDatabase("sample_mflix");
-var db = MflixDbContext.Create(database);
+
+var db = MflixDbContext.Create(client.GetDatabase("sample_mflix"));
 
 var movie = db.Movies.First(m => m.Title == "Back to the Future");
 Console.WriteLine(movie.Plot);
-
-movie.AdaptedFromBook = false;
-await db.SaveChangesAsync();
-
-
-var moviesCollection = database.GetCollection<Movie>("movies");
-Console.WriteLine("Before creating a new Index:");
-PrintIndexes();
-
-var moviesIndex = new CreateIndexModel<Movie>(Builders<Movie>.IndexKeys
-    .Ascending(m => m.Title)
-    .Ascending(x => x.Rated));
-await moviesCollection.Indexes.CreateOneAsync(moviesIndex);
-
-Console.WriteLine("After creating a new Index:");
-PrintIndexes();
-
-void PrintIndexes()
-{
-    var indexes = moviesCollection.Indexes.List();
-    foreach (var index in indexes.ToList())
-    {
-        Console.WriteLine(index);
-    }
-}
-
-
-//LINQ to find all PG-13 movies sorted by title and containing the work "shark" in their plot
-var myMovies = db.Movies.
-    Where(m => m.Rated == "PG-13" && m.Plot.Contains("shark")).
-    OrderBy(m => m.Title);
-
-foreach (var m in myMovies)
-{
-    Console.WriteLine(m.Title);
-}
-
-
-Movie myMovie1= new Movie {
-    Title = "The Rise of EF Core 1",
-    Plot = "Entity Framework (EF) Core is a lightweight, extensible, open source and cross-platform version of the popular Entity Framework data access technology.",
-    Rated = "G"
-};
-
-db.Movies.Add(myMovie1);
-await db.SaveChangesAsync();
-
-var dbContext2 = MflixDbContext.Create(database);
-dbContext2.Database.AutoTransactionBehavior = AutoTransactionBehavior.Never;
-var myMovie2 = new Movie { Title = "The Rise of EF Core 2" };
-dbContext2.Movies.Add(myMovie2);
-
-var myMovie3 = new Movie { Title = "The Rise of EF Core 3" };
-dbContext2.Movies.Add(myMovie3);
-await dbContext2.SaveChangesAsync();
-
 
 internal class MflixDbContext : DbContext
 {
@@ -102,7 +42,8 @@ internal class MflixDbContext : DbContext
 
 internal class Movie
 {
-    public ObjectId Id { get; set; }
+    [BsonId]
+    public ObjectId _id { get; set; }
 
     [BsonElement("title")]
     public string Title { get; set; }
@@ -112,10 +53,4 @@ internal class Movie
 
     [BsonElement("plot")]
     public string Plot { get; set; }
-
-    [BsonElement("adaptedFromBook")]
-    public bool? AdaptedFromBook { get; set; }
-
-    [Timestamp]
-    public long? Version { get; set; }
 }
